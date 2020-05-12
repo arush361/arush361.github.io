b6acccf8b0f621c7a44d2731ae52372564982fdd
Arush Sharma <Arush361@gmail.com>
2020-05-12 13:00:18 +0530

    see more modal added

--- _site/js/app.js
+++ _site/js/app.js
@@ -9,7 +9,33 @@ jQuery(document).ready(function(){
   displayHamburgerMenu();
 
   var animateTheseClasses = ['.section-header', '.project', '.skill'];
+  ///////
+  // Get the modal
+	var modal = document.getElementById("seeMoreModal");
 
+	// Get the button that opens the modal
+	var btn = document.getElementById("seeMore");
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
+	// When the user clicks the button, open the modal 
+	btn.onclick = function() {
+	  modal.style.display = "block";
+	}
+
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+	  modal.style.display = "none";
+	}
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+	  if (event.target == modal) {
+	    modal.style.display = "none";
+	  }
+	}
+  //////
   // Scroll to point
    $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
@@ -144,4 +170,5 @@ function openCity(evt, cityName) {
 }
 
 // Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+//document.getElementById("defaultOpen").click();
+
