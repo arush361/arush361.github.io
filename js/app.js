8b46d760eca19675c25ff928d3a3fcfff17ecab9
Arush Sharma <Arush361@gmail.com>
2020-05-17 12:42:24 +0530

    loader added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
+document.onreadystatechange = function() { 
+    if (document.readyState !== "complete") { 
+        document.querySelector("body").style.visibility = "hidden"; 
+        document.querySelector(".loader").style.visibility = "visible"; 
+    } else { 
+        document.querySelector(".loader").style.display = "none"; 
+        document.querySelector("body").style.visibility = "visible"; 
+    } 
+}; 
 jQuery(document).ready(function(){
   var viewPortWidth = jQuery(window).width();
   var viewPortHeight = jQuery(window).height();
